55e58ac2644f6639021a92a0e002544477302893
Jeremy Muñoz Torres <jeremy2918@gmail.com>
2022-06-11 23:50:33 -0400

    Removed torn paper

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -10,12 +10,6 @@ const Preview: FC<T> = ({ value, config }) => {
   return (
     <section id="preview" className="relative w-1/2 bg-old py-12 px-6">
       <img
-        alt="Torn Paper"
-        src="./torn.png"
-        className="w-full absolute left-0 top-0 rotate-180"
-      />
-
-      <img
         alt={config.logo.label}
         src={config.logo.img}
         className="max-w-48 max-h-48 mx-auto my-0"
@@ -32,12 +26,6 @@ const Preview: FC<T> = ({ value, config }) => {
           className="w-52 absolute bottom-10 left-4"
         />
       </div>
-
-      <img
-        alt="Torn Paper"
-        src="./torn.png"
-        className="w-full absolute left-0 bottom-0"
-      />
     </section>
   );
 };
